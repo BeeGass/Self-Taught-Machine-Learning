0f2280210c5f59f1f7af98b0550bbc2f9febe1a0
Bryan <bryank123@live.com>
2020-10-29 17:14:00 -0400

    added python project

---
+++ Project_1/PlotDist.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+import math
+import sys
+
+
+def NormalDistOne():
+    mu = 3
+    sigma = math.sqrt(1)
+    x = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
+    plt.plot(x, stats.norm.pdf(x, mu, sigma))
+    plt.show()
+
+def NormalDistTwo():
+    mu = 3
+    sigma = math.sqrt(10)
+    x = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
+    plt.plot(x, stats.norm.pdf(x, mu, sigma))
+    plt.show()
+
+def main(argv):
+    print(argv)
+    if int(argv) == 0:
+        print("running DistOne")
+        NormalDistOne()
+    elif int(argv) == 1:
+        print("running DistTwo")
+        NormalDistTwo()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
